cbfc48749855df05b0144f89706b8f775b7a368e
ntm1504 <56552457+ntm1504@users.noreply.github.com>
2023-09-26 10:44:20 +0700

    Update pong2.h
    Add score count function

--- inc/pong2.h
+++ inc/pong2.h
@@ -1,10 +1,14 @@
-#include "../inc/framebf.h"
-#include "../inc/uart1.h"
+#ifndef PONG2_H
+#define PONG2_H
+
+#include "framebf.h"
+#include "uart1.h"
+#include "delay.h"
 
 typedef struct {
     int x, y;
+    int radius;
     int speed_x, speed_y;
-    int attr;
 } Ball;
 
 typedef struct {
@@ -13,9 +17,19 @@ typedef struct {
     int speed;                        //speed in y-direction
 } Paddle;
 
+int player_score, cpu_score;
+
+typedef enum{
+    menu,
+    play
+}state;
+
 void DrawBall (Ball ball);
 void UpdateBall(Ball *ball);
+void UpdateScore(int player_score, int cpu_score, Ball *ball);
 void DrawPaddle(Paddle paddle);
 void UpdatePaddle(Paddle *paddle);
 void UpdateCpuPaddle(Paddle *cpu, int ball_y);
 void game();
+
+#endif
